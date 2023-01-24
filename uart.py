1d41f4242a3e044af59e97896ed12e1f7d44b4f8
Olek Kopijka <aleksander.kopijka@gmail.com>
2023-01-24 20:09:18 +0100

    Działa odbieranie danych po uarcie Dziala ustawianie zakresów napięć kanałów oraz częstotliwości TPM'a od convst UWAGA, dużo zakomentowanych funkcji!!! Kod nie wysylda danych co chwile!

--- uart.py
+++ uart.py
@@ -2,8 +2,9 @@ from serial import Serial
 from time import sleep
 
 ser = Serial("COM13", 115200)
-helo = "hello"
+helo = "h"
 print("KURWAAAAAAAAAAAAAAAA_ZAP2")
 while True: 
-    x = ser.read(4)
+    x = ser.read()
     print(x)
+    ser.write(unicode("h"))
